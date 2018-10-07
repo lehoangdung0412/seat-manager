3c13dccc01e4767acbd57b8a74d0555ba8c2cd16
lehoangdung0412 <lehoangdung.hcmus@gmail.com>
2018-10-08 01:29:49 +0700

    add model Seat

--- seat_reservation/models.py
+++ seat_reservation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Seat(models.Model):
+    code = models.CharField(max_length=255)
+    is_disabled = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.is_disabled:
+            return '{} (Disabled)'.format(self.code)
+        return self.code
+
